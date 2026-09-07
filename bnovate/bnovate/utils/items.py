8788ab2d02627a786faf8c4a0031942426016a4e
Douglas Watson <douglas.watson@bnovate.com>
2026-09-07 17:15:23 +0200

    Item: continued WIP on item API

--- bnovate/bnovate/utils/items.py
+++ bnovate/bnovate/utils/items.py
@@ -4,6 +4,7 @@
 
 import frappe
 from frappe.client import attach_file
+from frappe.exceptions import DuplicateEntryError 
 
 def get_highest_item_code(prefix=1):
     """ Return highest item code in a naming series """
@@ -23,7 +24,12 @@ WHERE item_code LIKE "{prefix}%"
 
 @frappe.whitelist()
 def get_next_item_code(prefix):
-    """ Return next item code in naming series, with .01 suffix """
+    """ Return next item code in naming series, with .01 suffix .
+
+    Warning: avoid long prefixes, as unexpected behaviour may occur: 
+    consider if item 200199 and 200200 exist. get_next_item_code('2001') will return 200200.01, which already exists
+    
+    """
 
     last_code = get_highest_item_code(prefix)
     if not last_code:
@@ -35,6 +41,29 @@ def get_next_item_code(prefix):
         frappe.throw("Highest existing item can't be converted into a number: ", last_code)
     
     return "{}.01".format(last_code + 1)
+
+
+@frappe.whitelist()
+def get_item_codes(prefix):
+    """ Return list of all item codes matching the prefix.
+    
+    For exact match, set prefix to the full item code:
+    
+    get_item_codes("100000.01")  # returns ["100000.01"]
+    get_item_codes("100000")  # returns ["100000.01", "100000.02", ...]
+    get_item_codes("1")  # returns ["100000.01", "100000.02", ..., "1000001.01", ...]
+
+    """
+
+    query = """
+    --  --sql
+SELECT item_code
+FROM `tabItem`
+WHERE item_code LIKE "{prefix}%"
+;
+    """.format(prefix=prefix)
+    data = frappe.db.sql(query, as_dict=True)
+    return [d.item_code for d in data]  
 
 
 @frappe.whitelist()
@@ -52,16 +81,30 @@ def set_naming_series(prefix, number=0):
 def get_naming_series():
     return frappe.db.sql("SELECT * FROM `tabSeries`", as_dict=True)
 
-    
 @frappe.whitelist()
-def create_item(item_name, description, item_code=None, prefix="1", image=None):
-    """ Create item based on a naming series 
+def get_item(item_code):
+    """ Return item details for a given item code """
+    item = frappe.get_doc("Item", item_code).as_dict()
+    item["attachments"] = get_attachments(item_code)
+    return item
+
+def get_attachments(item_code):
+    return frappe.get_all(
+        "File",
+        filters={
+            "attached_to_doctype": "Item",
+            "attached_to_name": item_code,
+        },
+        fields=["name", "file_name", "file_url", "is_private"],
+        order_by="creation asc",
+    )
 
     
-    Image should be a file attachment and will be set as the item image if provided.
-    Additional form data supported:
-    - image [file]: adds image as attachemnt and sets as cover image
-    - any other file: adds as attachment
+@frappe.whitelist()
+def create_item(item_name, description, item_code=None, prefix="1"):
+    """ Create item based on a naming series 
+
+    Any files included in the form data will be attached to the item. If the form value name is "image", it will be attached to the image field of the item.
     
     """
 
@@ -82,6 +125,29 @@ def create_item(item_name, description, item_code=None, prefix="1", image=None):
     })
     item.insert()
 
+    upload_attachments(item_code)
+
+    return item.name
+
+    
+@frappe.whitelist()
+def upload_attachments(item_code):
+    """ Upload attachments to an item.  
+
+     Any files included in the form data will be attached to the item. 
+     If the form value name is "image", it will be attached to the image field of the item.
+   
+    """
+
+
+    if not item_code:
+        frappe.throw("Item code is required to upload attachments.")
+
+    if not frappe.db.exists("Item", item_code):
+        frappe.throw("Item does not exist: {0}".format(item_code))
+
+    
+
     # Check for attachments. Special treatent if form value name is "image".
     for name, filestorage in frappe.request.files.items():
         print(name, filestorage)
@@ -91,4 +157,19 @@ def create_item(item_name, description, item_code=None, prefix="1", image=None):
 
         file = attach_file(filename, content, "Item", item_code, docfield=docfield, is_private=1)
 
-    return item.name
+
+        
+@frappe.whitelist()
+def update_item(item_code, **kwargs):
+    """ Update item details for a given item code. """
+    changes = {"item_code": item_code}
+    item = frappe.get_doc("Item", item_code)
+    for key, value in kwargs.items():
+        if hasattr(item, key):
+            changes[key] = value
+            setattr(item, key, value)
+    item.save()
+
+    upload_attachments(item_code)
+
+    return changes
